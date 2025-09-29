282cfda6a131ed537d5dbf096bb654852caecb14
burberryonn <swaronworld2@mail.ru>
2025-09-29 22:08:08 +0300

    add next.configs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,7 @@ const configuredDevOrigins = process.env.NEXT_DEV_ALLOWED_ORIGINS?.split(",")
 const nextConfig = {
   allowedDevOrigins: Array.from(
     new Set([
+      process.env.ALLOWED_ORIGINALS,
       "localhost",
       "127.0.0.1",
       "https://yt-download-wlmo-git-main-burberryonns-projects-dcec7371.vercel.app/",
